1e4b29b2c738257f6b1e2a775728297797db6db1
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-04 22:27:19 +0545

    Node globalObject

---
+++ globalObject.js
@@ -0,0 +1,7 @@
+let time = 0;
+
+setInterval(function(){
+    time += 2
+    console.log(time + ' Seconds have passed')
+}, 2000);
+// Message will be appear in every 2 seconds in terimal while running app.
