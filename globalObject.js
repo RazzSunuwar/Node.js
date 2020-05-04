10d6cccdb5496a2c5eb0dd6db6ed72c90883451e
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-04 22:47:57 +0545

    Node globalObject

--- globalObject.js
+++ globalObject.js
@@ -1,7 +1,20 @@
 let time = 0;
 
-setInterval(function(){
+// setInterval(function(){
+//     time += 2
+//     console.log(time + ' Seconds have passed')
+// }, 2000);
+// // Message will be appear in every 2 seconds in terimal while running app.
+
+let timer = setInterval(function(){
     time += 2
-    console.log(time + ' Seconds have passed')
-}, 2000);
-// Message will be appear in every 2 seconds in terimal while running app.
+    console.log(time + ' seconds have passed');
+    if(time >= 5){
+        clearInterval(timer);
+    }
+}, 3000);
+
+// Run 2,4 & 6 on console
+
+console.log(__dirname); //this will tell the location of present file
+console.log(__filename); //this will show the directory and files
