5544d9157ae93f11bfce367075c6a290ea10e957
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-05 20:51:36 +0545

    Function Expressions

---
+++ FunExpression.js
@@ -0,0 +1,11 @@
+// // normal function statement
+// function sayHi(){
+//     console.log("Hello World");
+// }
+// sayHi();
+
+// Functional Expression
+let sayHello = function(){
+    console.log("Hello Universe");
+}
+sayHello();
