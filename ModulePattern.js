1007e5846742def6473a60f936722f738233a791
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-05 21:39:36 +0545

    Module Pattern

---
+++ ModulePattern.js
@@ -0,0 +1,6 @@
+const { counter, sum, PI } = require('./ModulePattern0');
+
+
+console.log(counter(["Ramsey", "Andy", "Hutson"]));
+console.log(sum(2, 3));
+console.log(sum(7, PI));
---
+++ ModulePattern0.js
@@ -0,0 +1,12 @@
+let counter = function(arr){
+    return "There are "+ arr.length + "numbers of elements in the array"
+};
+
+let sum = function(a, b){
+    return `The total of elements is ${a+b}`
+};
+
+let PI = 3.14;
+
+module.exports = { counter, sum, PI };
+
