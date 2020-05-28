4f0a2b8df30030a2b9b3b514f0bbfaf319b8605d
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-28 21:21:28 +0545

    File System asyn write file

--- fileSystem.js
+++ fileSystem.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 
 // Sync 
 var readme = fs.readFileSync("README.md", "utf8");
-console.log(readme);
+// console.log(readme);
+
+// Write file, filename with writeMe.txt
+fs.writeFileSync('writeMe.txt', readme);
 
 
