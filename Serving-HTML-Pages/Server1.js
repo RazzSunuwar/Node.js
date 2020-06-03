93df997b54b586dd3601e36f56ff41722070116c
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-03 23:08:50 +0545

    Basic Routing

--- Serving-HTML-Pages/Server1.js
+++ Serving-HTML-Pages/Server1.js
@@ -4,12 +4,22 @@ const port = process.env.PORT || 3001;
 
 const Server = http.createServer((req, res) => {
     console.log('Request url:', req.url);
+
     if(req.url === '/home' || req.url === '/'){
     res.writeHead(200, {'Content-Type': 'text/html'});
     fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } else if(req.url === '/contact' || req.url === '/'){
+    } 
+    else if(req.url === '/contact' || req.url === '/'){
         res.writeHead(200, {'Content-Type': 'text/html'});
         fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }
+    else if(req.url === '/api/ninjas'){
+        let ninjas = [{name: 'Kevin', age: 30}, {name: 'Brat', age: '26'}]
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/error.404.html').pipe(res);      
     }
 });
 
