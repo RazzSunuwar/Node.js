42f109985b70aa2a497662639f0d967b338824bc
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-05 22:27:41 +0545

    Read and Write files

--- fileSystem.js
+++ fileSystem.js
@@ -1,10 +1,27 @@
 const fs = require("fs");
 
-// Sync 
-var readme = fs.readFileSync("README.md", "utf8");
-// console.log(readme);
 
-// Write file, filename with writeMe.txt
-fs.writeFileSync('writeMe.txt', readme);
+// Sync 
+// var readme = fs.readFileSync("README.md", "utf8");
+// // console.log(readme);
+
+// // Write file, filename with writeMe.txt
+// fs.writeFileSync('writeMe.txt', readme);
+
+
+
+// Async
+fs.readFile("ReadFile.txt", "utf8", function(err, data){
+
+    // console.log(data);
+    fs.writeFile('WriteFiles.txt', data, (err, done) =>{
+        if(err){
+            console.log('Error writing message', err)
+        } else {
+            console.log('Success writing code');
+        }
+    });
+});
+
 
 
