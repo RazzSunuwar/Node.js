3782e99ada0ff9f12581f8b950d988f60cc4dae0
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-05 22:31:52 +0545

    Created directory, read and write files

--- Directory.js
+++ Directory.js
@@ -4,4 +4,24 @@ const fs = require("fs");
 // fs.mkdirSync("FileSystem");
 
 // Remove dir sync
-fs.rmdirSync("FileSystem");
+// fs.rmdirSync("FileSystem");
+
+
+
+// Async
+fs.mkdir("Directory", function(){
+    fs.readFile("README.md", "utf8", function(err, data){
+        fs.writeFile("./Directory/writeMe.txt", data, function(err, done){
+            if(err){
+                console.log("Error in creating new file", err)
+            } return console.log('Success in creating new file');
+        });
+    });
+});
+
+
+// Remove file and directory
+// fs.unlink('./directory/writeMe.txt', function(){
+//     fs.rmdir("Directory");
+// })
+
