4628cfc5a5cd58e90a665cb568ff436d34455cdb
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-06 23:28:52 +0545

    Remove Folder

--- Directory.js
+++ Directory.js
@@ -9,19 +9,23 @@ const fs = require("fs");
 
 
 // Async
-fs.mkdir("Directory", function(){
-    fs.readFile("README.md", "utf8", function(err, data){
-        fs.writeFile("./Directory/writeMe.txt", data, function(err, done){
-            if(err){
-                console.log("Error in creating new file", err)
-            } return console.log('Success in creating new file');
-        });
-    });
-});
+// fs.mkdir("Folder", function(){
+//     fs.readFile("README.md", "utf8", function(err, data){
+//         fs.writeFile("./Folder/writeMe.txt", data, function(err, done){
+//             if(err){
+//                 console.log("Error in creating new file", err)
+//             } return console.log('Success in creating new file');
+//         });
+//     });
+// });
 
 
 // Remove file and directory
-// fs.unlink('./directory/writeMe.txt', function(){
-//     fs.rmdir("Directory");
-// })
+fs.unlink('./Folder/writeMe.txt', function(err, data){
+    fs.rmdir("Folder", data, function(err, done){
+        if(err){
+            console.log("Error in remove folder", done)
+        } return console.log('Success in delete folder');
+    });
+});
 
