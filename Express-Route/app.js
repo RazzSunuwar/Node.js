08ecacd79f751c3af620766ff8156da12093aff9
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-07 10:31:40 +0545

    Template Engine ejs

--- Express-Route/app.js
+++ Express-Route/app.js
@@ -3,16 +3,19 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send('This is the homepage');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
-    res.send('This is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
-app.get('/profile/:id', function(req, res){
-    res.send('You requested to see a profile with the id of ' + req.params.id);
+app.get('/profile/:name', function(req, res){
+    let data = {age: 29, job: 'Ninja'}
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(port, (err, done) => {
